bf02fcc0dd230d0966bd0af2e7ad3810c23a5886
Mayank Saraswat <mayanksaraswat497@gmail.com>
2022-11-29 21:18:04 +0530

    code added

---
+++ src/main/java/org/example/DownloadManager.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.example.config.AppConfig;
+import org.example.models.FileInfo;
+
+import java.io.File;
+
+public class DownloadManager {
+    @FXML
+    private TableView<FileInfo> tableView;
+
+    @FXML
+    private TextField urlTextField;
+    public int index = 0;
+    @FXML
+    void downloadButtonClicked(ActionEvent event) {
+
+        String url = urlTextField.getText().trim();
+        String filename= url.substring(url.lastIndexOf("/") +1);
+        String status="STARTING";
+        String action="OPEN";
+        String path = AppConfig.DOWNLOAD_PATH+ File.separator+filename;
+        FileInfo file= new FileInfo((index+1),filename,url,status,action,path);
+        this.index += 1;
+        DownloadThread thread = new DownloadThread(file,this);
+        this.tableView.getItems().add(file);
+        thread.start();
+        this.tableView.refresh();
+    }
+
+    public void updateUI(FileInfo metaFile) {
+        System.out.println(metaFile);
+        this.tableView.getItems().set(metaFile.getIndex() - 1, metaFile);
+        this.tableView.refresh();
+        System.out.println("---------------------");
+    }
+    @FXML
+    public void initialize(){
+        System.out.println("View initialized");
+        TableColumn<FileInfo, String> sn = (TableColumn<FileInfo, String>) this.tableView.getColumns().get(0);
+        sn.setCellValueFactory(p->{
+            int index = p.getValue().getIndex();
+            SimpleStringProperty simpleStringProperty = new SimpleStringProperty(index+"");
+            return simpleStringProperty;
+        });
+        TableColumn<FileInfo, String> fileName = (TableColumn<FileInfo, String>) this.tableView.getColumns().get(1);
+        fileName.setCellValueFactory(p->{
+            return p.getValue().nameProperty();
+        });
+        TableColumn<FileInfo, String> url = (TableColumn<FileInfo, String>) this.tableView.getColumns().get(2);
+        url.setCellValueFactory(p->{
+            return p.getValue().urlProperty();
+        });
+        TableColumn<FileInfo, String> status = (TableColumn<FileInfo, String>) this.tableView.getColumns().get(3);
+        status.setCellValueFactory(p->{
+            return p.getValue().statusProperty();
+        });
+        TableColumn<FileInfo, String> action = (TableColumn<FileInfo, String>) this.tableView.getColumns().get(4);
+        action.setCellValueFactory(p->{
+            return p.getValue().actionProperty();
+        });
+    }
+}
---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,6 @@
+package org.example.config;
+
+public class AppConfig {
+    public static final String DOWNLOAD_PATH= "C:\\Users\\Mayank Saraswat\\Desktop\\Coding\\Projects";
+    //other config info
+}
